8d4f0c064632aee40f45e517c5701c91ca70a59b
PedHr <pedrohe313131@gmail.com>
2025-08-26 00:08:19 -0300

    pagina de stock criada e integrada com o banco de dados

--- src/routes/stock_page.py
+++ src/routes/stock_page.py
@@ -1,9 +1,24 @@
-from flask import Flask, Blueprint, render_template, url_for
+from flask import Flask, Blueprint, render_template, jsonify
 import mysql.connector
+from supabase import create_client, Client
 from src.config import SQL_CREDENTIALS
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 stock_page = Blueprint('stock_page', __name__)
 
 @stock_page.route('/stock')
 def teste():
-    return "<p> stock Page </p>"
+    return render_template("stock_page.html")
+
+@stock_page.route('/stock/data')
+def buscar_estoque():
+    response = supabase.table('produto').select('*').execute()
+    dados = response.data
+    return jsonify({"data":dados})
